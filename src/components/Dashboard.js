69c8fdcf960549832416405afed6143d7ce52def
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-01-15 15:00:15 -0800

    updates dashboard syntax

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,8 +4,8 @@ export default function Dashboard({loggedInStatus, user, customer}) {
   return (
     <div>
       <h1>Dashboard</h1>
-      {user === {}} && <h2>Welcome Customer {customer.email}</h2>
-      {customer === {}} && <h2>Welcome {user.email}</h2>
+      {user === {} && <h2>Welcome Customer {customer.email}</h2>}
+      {customer === {} && <h2>Welcome {user.email}</h2>}
       <h2>Status: {loggedInStatus}</h2>
     </div>
   )
