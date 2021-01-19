288f1644d17a7b5578331943e0ba3871ed8b8ba1
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-01-19 12:55:02 -0800

    adds stripe import

--- src/containers/ProductContainer.js
+++ src/containers/ProductContainer.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { loadStripe } from '@stripe/stripe-js';
+const stripePromise = loadStripe('pk_test_51I8VgBLQ8SRg06CGvE7ORHZK9TXklhMoJ1RGACj9PUgozDhU8IjjUaJWeczZM3SW6bVZSJC0vrhuzb5QDOohvLXd00YXy0xfa9');
+
 
 export default class ProductContainer extends Component {
   render() {
