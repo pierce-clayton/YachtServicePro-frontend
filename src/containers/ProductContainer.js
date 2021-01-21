c61cffb4ac29d6d3b46b19a1f4325f54db42e6d7
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-01-21 15:18:50 -0800

    adds one time checkout

--- src/containers/ProductContainer.js
+++ src/containers/ProductContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { loadStripe } from '@stripe/stripe-js';
-const stripePromise = loadStripe('pk_test_51I8VgBLQ8SRg06CGvE7ORHZK9TXklhMoJ1RGACj9PUgozDhU8IjjUaJWeczZM3SW6bVZSJC0vrhuzb5QDOohvLXd00YXy0xfa9');
+const stripePromise = window.Stripe('pk_test_51I8VgBLQ8SRg06CGvE7ORHZK9TXklhMoJ1RGACj9PUgozDhU8IjjUaJWeczZM3SW6bVZSJC0vrhuzb5QDOohvLXd00YXy0xfa9');
 import Product from '../components/Product'
 
 
@@ -17,7 +17,19 @@ export default class ProductContainer extends Component {
     })
   }
   handlePurchase = (product, interval) => {
-    console.log(product, interval)
+    const price_obj = product.prices.filter(price => price.stripe_type === interval).pop()
+    axios.post('https://backend.baracus.rocks/checkout_sessions.json',{
+      price_id: price_obj.stripe_id,
+      customer_id: this.props.customer.stripe_id,
+      success_url: 'https://ysp.baracus.rocks/dashboard',
+      cancel_url: 'https://ysp.baracus.rocks/',
+      stripe_mode: interval === 'one_time' ? 'payment' : 'subscription'
+
+    }, {withCredentials: true})
+    .then(response => {
+      return stripePromise.redirectToCheckout({sessionId: response.data.stripe_id})
+    })
+    console.log(stripePromise)
   }
   
   render() {
